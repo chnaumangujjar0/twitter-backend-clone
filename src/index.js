fba73127c67c5277615e89b33af7c97ccb3f8e12
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-06-09 23:43:08 +0500

    connect db and add utils

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import dotenv from "dotenv"
+dotenv.config();
+import express from "express";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Error : ",err)
+        throw err
+    })
+    
+    app.listen(process.env.PORTt || 8000 ,()=>{
+        console.log(`Server is listening on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB error :",error)
+})
